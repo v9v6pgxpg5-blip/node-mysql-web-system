0c752b8e22cc8dc130d6b003ef970e3eb64e1d91
v9v6pgxpg5-blip <v9v6pgxpg5@privaterelay.appleid.com>
2026-08-12 06:38:36 +0000

    サインイン機能を作成しよう

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+
   knex("tasks")
     .select("*")
     .then(function (results) {
-      console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
@@ -36,5 +38,6 @@ router.post('/', function (req, res, next) {
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 
 module.exports = router;
