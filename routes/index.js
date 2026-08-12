5f981506aa06c6b92da955f6218dd0c1f12c9dae
v9v6pgxpg5-blip <v9v6pgxpg5@privaterelay.appleid.com>
2026-08-12 06:04:53 +0000

    mysqlモジュールを用いてデータベースに接続できるようにしよう

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '[password]',
+  database: 'todo_app'
+});
 
 let todos = [];
 
@@ -11,6 +19,13 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo = req.body.add;
   todos.push(todo);
   res.redirect('/');
