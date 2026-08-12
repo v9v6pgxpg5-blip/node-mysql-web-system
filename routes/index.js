1ee2b2dc78fc4e5c561905eb5ae2de5dd6a398ac
v9v6pgxpg5-blip <v9v6pgxpg5@privaterelay.appleid.com>
2026-08-12 05:40:51 +0000

    アプリケーションを起動して処理の流れを確認しよう

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+let todos = [];
+
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ToDo App' });
+});
+
+router.post('/', function(req, res, next) {
+  const todo = req.body.add;
+  todos.push(todo);
   res.render('index', { title: 'ToDo App' });
 });
 
