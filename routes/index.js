9e1e69536717022bcba9c32d1b3968e9ad0ab850
v9v6pgxpg5-blip <v9v6pgxpg5@privaterelay.appleid.com>
2026-08-12 06:09:58 +0000

    ToDoタスクをデータベースから取得して表示させる処理を実装しよう。

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
-let todos = [];
-
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -11,12 +9,18 @@ const connection = mysql.createConnection({
   database: 'todo_app'
 });
 
-
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  connection.query(
+    `select * from tasks;`,
+    (error, results) => {
+      console.log(error);
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    }
+  );
 });
 
 router.post('/', function (req, res, next) {
