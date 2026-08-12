7fb5c595fc3ad8f446127297f29e651ab7d967eb
v9v6pgxpg5-blip <v9v6pgxpg5@privaterelay.appleid.com>
2026-08-12 13:49:18 +0000

    passport認証の動作確認をしよう

--- config/passport.js
+++ config/passport.js
@@ -8,17 +8,19 @@ const secret = "secretCuisine123";
 
 module.exports = function (app) {
   passport.serializeUser(function (user, done) {
+    console.log("serializeUser");
     done(null, user.id);
   });
 
   passport.deserializeUser(async function (id, done) {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (error) {
-      done(error, null);
-    }
-  });
+console.log("deserializeUser");
+try {
+const user = await User.findById(id);
+done(null, user);
+} catch (error) {
+done(error, null);
+}
+});
 
   passport.use(new LocalStrategy({
       usernameField: "username",
@@ -55,5 +57,6 @@ module.exports = function (app) {
     })
   );
 
+  app.use(passport.initialize());
   app.use(passport.session());
 };
